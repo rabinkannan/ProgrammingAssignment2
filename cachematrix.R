4bed2040c7d0fda6fbcb5c53f7ba63b8f1a4875d
Kannan <rabinkannan@yahoo.com>
2015-10-24 18:29:42 +0530

    Solution for Prog.Assignment# 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function enables caching of its returned value, so that for any more request
+## for the value, it will be fetched from cache, provided the passed matrix has not changed
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invMatrix <- NULL
+  setMatrix <- function(y) {
+    x <<- y
+    invMatrix <<- NULL
+  }
+  getMatrix <- function() x
+  setInvMatrix <- function(inverse) invMatrix <<- inverse
+  getInvMatrix <- function() invMatrix
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setInvMatrix = setInvMatrix,
+       getInvMatrix = getInvMatrix)
 }
 
-
-## Write a short comment describing this function
-
+## This function would find the inverse of passed matrix. If the inverse is available in the
+## cache, the cached inverse will be returned. If so, there will be message as "From cache."
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  invMatrix <- x$getInvMatrix()
+  if (!is.null(invMatrix)) {
+    message("From cache.")
+    return(invMatrix)
+  }
+  mymatrix <- x$getMatrix()
+  invMatrix <- solve(mymatrix, ...)
+  x$setInvMatrix(invMatrix)
+  invMatrix
 }
